d420823a2f24c9ec606fb28f198391de678cb0b5
Southgirl <425202588@qq.com>
2017-11-27 10:41:46 +0800

    底部抽离

--- company/src/main/webapp/js/homePage.js
+++ company/src/main/webapp/js/homePage.js
@@ -18,4 +18,6 @@ $(function(){
        $(".loginBox").hide();
         $(".lightBox").hide();
     });  
+    $("#header").load('../index/common-html/common-footer.html', function (data, status, xhr) {});
+    
 })
