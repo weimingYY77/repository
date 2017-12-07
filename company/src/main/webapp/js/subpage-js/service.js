b4cfc461222dfba2d128fc9e6449a29ada79a5cf
liyuan <1428571247@qq.com>
2017-12-07 13:38:21 +0800

    添加主页线条滑动

--- company/src/main/webapp/js/subpage-js/service.js
+++ company/src/main/webapp/js/subpage-js/service.js
@@ -1,9 +1,16 @@
 $(function(){
-	console.log($(window).scrollTop())
+	console.log($(".head_top").height())
 	$(window).scroll(function(){
-/*	if ($(window).scrollTop()>700) {
-			
-		}*/
+		// 当滚动条滚动超过顶部时，Logo变成黑色，否则logo为白色
+	if ($(window).scrollTop() >= $(".head_top").height()) {
+			$(".img_black").show();
+			$(".img_white").hide();
+		}
+		else{
+			$(".img_black").hide();
+			$(".img_white").show();
+		}
+		// 根据滚动条滚动给四块内容添加动画
 		if ($(window).scrollTop()>1000) {
 			$("#block_one").addClass('visibility_play1');
 		    $("#blocktxt_one").addClass('program_play');
