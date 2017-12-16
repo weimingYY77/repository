155a55cbb4d8d9d3f746f89111a53ae72397c877
Southgirl <425202588@qq.com>
2017-12-16 15:11:08 +0800

    1

---
+++ company/src/main/webapp/js/common-js/common.js
@@ -0,0 +1,4 @@
+$(function(){
+        $("#test").load("http://www.sumchance.com/src/main/webapp/index//common-html/footer.html");
+        $("#nav").load("http://www.sumchance.com/src/main/webapp/index/common-html/nav.html");
+    });
