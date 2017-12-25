c3af2fc52984c1806bfba172d4b41ff6a88e41fd
liyuan <1428571247@qq.com>
2017-12-25 16:29:29 +0800

    yuntuiguang

---
+++ company-copy/src/main/webapp/controller/js/common-js/common.js
@@ -0,0 +1,31 @@
+$(function(){
+  // 导航栏搜索框
+ $(".search").click(function(){
+    $(".search_box").fadeIn(500);
+    $(".search").fadeOut(500);
+ })
+   $(".search_txt").keydown(function(){
+      $(".close").show();
+   })
+   $(".close").click(function(){
+     $(".search_txt").val(' ');
+     $(".close").hide();
+   });
+   /* 导航下拉列表*/
+$(".drop-down").mouseenter(function() {
+      $(this).children("div").stop().fadeIn("slow");
+});
+   $(".drop-down").mouseleave(function() {
+      $(this).children("div").stop().fadeOut("slow");
+});
+  // 折叠菜单栏
+	 $('.inactive').click(function(){
+       if($(this).children('ul').css('display') == 'none'){
+           $(this).children('ul').slideDown(600);
+           $(this).children("a").children("span").removeClass("rjt").addClass("bjt");
+       }else{
+           $(this).children('ul').slideUp(600);
+           $(this).children("a").children("span").removeClass("bjt").addClass("rjt");
+       }
+     })
+})
--- company-copy/src/main/webapp/controller/js/index.js
+++ company-copy/src/main/webapp/controller/js/index.js
@@ -1,39 +1,4 @@
 $(function(){
-  // 导航栏搜索框
- $(".search").click(function(){
-    $(".search_box").fadeIn(500);
-    $(".search").fadeOut(500);
- })
-   $(".search_txt").focus(function(){
-      if ($(".search_txt").val()!=" ") {
-         console.log(123);
-        $(".close").fadeIn();
-      }else{
-         console.log(456);
-         $(".close").fadeOut();
-      }
-   })
-   $(".close").click(function() {
-    console.log(123);
-     $(".search_txt").val() = "";
-   });
-   /* 导航下拉列表*/
-$(".drop-down").mouseenter(function() {
-      $(this).children("div").stop().fadeIn("slow");
-});
-   $(".drop-down").mouseleave(function() {
-      $(this).children("div").stop().fadeOut("slow");
-});
-  // 折叠菜单栏
-	 $('.inactive').click(function(){
-       if($(this).children('ul').css('display') == 'none'){
-           $(this).children('ul').slideDown(600);
-           $(this).children("a").children("span").removeClass("rjt").addClass("bjt");
-       }else{
-           $(this).children('ul').slideUp(600);
-           $(this).children("a").children("span").removeClass("bjt").addClass("rjt");
-       }
-     })
    // 选项卡切换
     $(".box_list li").click(function(){
         var index = $(".box_list li").index(this)
