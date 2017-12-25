b37057f3ff19a340be9ec3bab8fafb4e0e826899
liyuan <1428571247@qq.com>
2017-12-25 09:14:28 +0800

    导航修改

--- company-copy/src/main/webapp/controller/js/index.js
+++ company-copy/src/main/webapp/controller/js/index.js
@@ -1,21 +1,38 @@
 $(function(){
+  // 导航栏搜索框
+ $(".first_search").click(function(){
+    $(".search_box").fadeIn(500);
+    $(".search").fadeOut(500);
+ })
+   $(".search_txt").focus(function(){
+      if ($(".search_txt").val()!="") {
+         console.log(123)
+        $(".close").show();
+      }else{
+         console.log(456);
+         $(".close").hide();
+      }
+   })
+ // 导航下拉列表
+  $(".drop-down").mouseenter(function() {
+      $(this).children("div").stop().slideDown("slow");
+  });
+   $(".drop-down").mouseleave(function() {
+      $(this).children("div").stop().slideUp("slow");
+  });
+  // 折叠菜单栏
 	 $('.inactive').click(function(){
-       if($(this).siblings('ul').css('display')=='none'){
-           $(this).siblings('ul').slideDown(100);
+       if($(this).children('ul').css('display') == 'none'){
+           $(this).children('ul').slideDown(600);
+           $(this).children("a").children("span").removeClass("rjt").addClass("bjt");
        }else{
-           $(this).siblings('ul').slideUp(100);
+           $(this).children('ul').slideUp(600);
+           $(this).children("a").children("span").removeClass("bjt").addClass("rjt");
        }
      })
-     $('.inactive').click(function(){
-     	if ($(this).children("span").attr("class")=="rjt") {
-     		$(this).children("span").removeClass("rjt").addClass("bjt");
-     	}else{
-           $(this).children("span").removeClass("bjt").addClass("rjt");
-     	}
-     })
-
+   // 选项卡切换
     $(".box_list li").click(function(){
-        var index=$(".box_list li").index(this)
+        var index = $(".box_list li").index(this)
         $(this).addClass("active").siblings().removeClass("active")
         $(".service_content").eq(index).addClass("active").siblings().removeClass("active")
     })
