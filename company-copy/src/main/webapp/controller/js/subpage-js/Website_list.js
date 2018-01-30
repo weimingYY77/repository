85242fd4097fa1f83b35f1fdab9aac896624fedc
bainian.yu <320078639@qq.com>
2018-01-30 15:34:29 +0800

    1111

--- company-copy/src/main/webapp/controller/js/subpage-js/Website_list.js
+++ company-copy/src/main/webapp/controller/js/subpage-js/Website_list.js
@@ -40,15 +40,11 @@ $(function(){
 	    	   	var code = userId;
 				var leng=code.length;  //定义长度
 				if(leng==1){
-				code="00000"+code;
-				}else if(leng==2){
-				code="0000"+code;
-				}else if(leng==3){
-				code="000"+code;
-				}else if(leng==4){
-				code="00"+code;
-				}else if(leng==5){
-				code="0"+code;
+					code="000"+code;
+					}else if(leng==2){
+					code="00"+code;
+					}else if(leng==3){
+					code="0"+code;
 				}
 				$("#userId").html(code);
    	}else{
