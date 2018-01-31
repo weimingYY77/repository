921e22e866836386746882940664afe973f98841
bainian.yu <320078639@qq.com>
2018-01-31 09:51:19 +0800

    11

--- company-copy/src/main/webapp/controller/js/common-js/common.js
+++ company-copy/src/main/webapp/controller/js/common-js/common.js
@@ -43,6 +43,10 @@ $(".drop-down").mouseenter(function() {
   // 小选项卡
    $(".tab_list li").click(function(){
     var index = $(".tab_list li").index(this);
+        $(this).addClass("active").siblings().removeClass("active");
+  })
+   $(".tab_list li").click(function(){
+    var index = $(".tab_list li").index(this);
         $(".tab_content").eq(index).addClass("show_tab").siblings().removeClass("show_tab");
   })
 // 小切换按钮
