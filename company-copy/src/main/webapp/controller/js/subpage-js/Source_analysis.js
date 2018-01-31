1d2d249b58da25d7e9314bed1a4a82e800cb7b41
bainian.yu <320078639@qq.com>
2018-01-31 15:04:56 +0800

    111

--- company-copy/src/main/webapp/controller/js/subpage-js/Source_analysis.js
+++ company-copy/src/main/webapp/controller/js/subpage-js/Source_analysis.js
@@ -456,28 +456,12 @@ $(function(){
 				            label: {
 				                normal: {
 				                    formatter: '{b|{b}:}{c}\n{per|{d}%} ',
-				                    // backgroundColor: '#eee',
-				                    // borderColor: '#aaa',
-				                    // borderWidth: 2,
-				                    // borderRadius: 4,
-				                    // shadowBlur:3,
-				                    // shadowOffsetX: 2,
-				                    // shadowOffsetY: 2,
-				                    // shadowColor: '#999',
-				                    // padding: [0, 7],
 				                    rich: {
 				                        a: {
 				                            color: '#999',
 				                            lineHeight: 22,
 				                            align: 'right'
 				                        },
-				                        // abg: {
-				                        //     backgroundColor: '#333',
-				                        //     width: '100%',
-				                        //     align: 'right',
-				                        //     height: 22,
-				                        //     borderRadius: [4, 4, 0, 0]
-				                        // },
 				                        b: {
 				                            fontSize: 12,
 				                            lineHeight: 33
